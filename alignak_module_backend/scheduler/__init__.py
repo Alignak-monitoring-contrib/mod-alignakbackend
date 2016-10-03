ec471c067da6cdebcd1516857bfbd4f0b5ced2ae
Frédéric MOHIER <frederic.mohier@gmail.com>
2016-10-03 12:10:31 +0200

    Update typos in modules __init__ files

--- alignak_module_backend/scheduler/__init__.py
+++ alignak_module_backend/scheduler/__init__.py
@@ -20,7 +20,7 @@
 # along with Alignak.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-The objects package contains the scheduler module to manage retention and livestates.
+This package contains the scheduler module to manage retention of the live state.
 """
 
 from .module import properties, get_instance
